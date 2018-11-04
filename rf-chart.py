3b0815f03229459cbf9ae2e026c52368ea5b8f4f
Thomas Gish <jonmpqts@gatech.edu>
2018-11-04 09:20:15 -0800

    Let pyplot choose colors

--- rf-chart.py
+++ rf-chart.py
@@ -10,7 +10,7 @@ rf = joblib.load(rf_path)
 std = np.std([rf.feature_importances_ for tree in rf.estimators_], axis=0)
 indices = np.argsort(rf.feature_importances_)[::-1]
 
-plt.bar(range(len(rf.feature_importances_)), rf.feature_importances_[indices], color='b', yerr=std[indices], align='center')
+plt.bar(range(len(rf.feature_importances_)), rf.feature_importances_[indices], yerr=std[indices], align='center')
 plt.xticks(range(len(rf.feature_importances_)), indices)
 plt.xlim([-1, len(rf.feature_importances_)])
 plt.xlabel('Feature')
